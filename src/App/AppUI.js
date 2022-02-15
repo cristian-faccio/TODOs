6d3042861432d6dc82c245ed1fe4bc8aff9b945a
cristian-faccio <cristianarielfaccio@hotmail.com>
2022-02-15 18:51:07 -0300

    update

---
+++ src/App/AppUI.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { TodoContext } from '../TodoContext';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { TodoForm } from '../TodoForm';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { Modal } from '../Modal';
+import { TodoError } from '../TodoError';
+import { TodoLoading } from '../TodoLoading';
+import { TodoEmpty } from '../TodoEmpty';
+
+function AppUI() {
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+  } = React.useContext(TodoContext);
+  
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        {error && <TodoError error={error}/>}
+        {loading && <TodoLoading/>}
+        {(!loading && !searchedTodos.length) && <TodoEmpty/>git remote add origin git@github.com:cristian-faccio/TODOs.git
+git branch -M main
+git push -u origin main}
+        
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+
+      {!!openModal && (
+        <Modal>
+          <TodoForm />
+        </Modal>
+      )}
+
+      <CreateTodoButton
+        setOpenModal={setOpenModal}
+      />
+    </React.Fragment>
+  );
+}
+
+export { AppUI };
